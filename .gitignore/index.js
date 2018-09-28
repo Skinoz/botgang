3cd4ad8cce52491052e7f42ee890356f9a45147f
Skinoz <38363543+Skinoz@users.noreply.github.com>
2018-09-28 23:14:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-var anti_spam = require("discord-anti-spam");
+var anti_spam = require("anti-spam.js");
 var bot = new Discord.Client();
 var prefix = ("!");
 
